7b70065a85f18f5c37e64430471c95272b0ebecb
TJ Checketts <tylerchecketts@gmail.com>
2017-09-11 10:05:54 -0600

    add opaque bkgrnd

--- src/components/LifeCounter.js
+++ src/components/LifeCounter.js
@@ -30,15 +30,21 @@ class LifeCounter extends React.Component {
   render() {
     return (
       <div>
-        <h2> Magic the Gathering Life Counter</h2>
+        <h2> Magic the Gathering: Life Counter</h2>
         <h2>Current Health: {this.state.value}</h2>
-        <button style={{backgroundColor: 'orange'}} onClick={this.minusThree}>- 3</button>
-        <button style={{backgroundColor: 'orange'}} onClick={this.minusTwo}>- 2</button>
-        <button style={{backgroundColor: 'orange'}} onClick={this.minusOne}>- 1</button>
+        <button style={{backgroundColor: 'orange', margin: 5, padding: 5}} 
+                      onClick={this.minusThree}>- 3</button>
+        <button style={{backgroundColor: 'orange', margin: 5, padding: 5}} 
+                      onClick={this.minusTwo}>- 2</button>
+        <button style={{backgroundColor: 'orange', margin: 5, padding: 5}} 
+                      onClick={this.minusOne}>- 1</button>
         {' '}
-        <button style={{backgroundColor: 'lightgreen'}} onClick={this.plusOne}>+ 1</button>
-        <button style={{backgroundColor: 'lightgreen'}} onClick={this.plusTwo}>+ 2</button>
-        <button style={{backgroundColor: 'lightgreen'}} onClick={this.plusThree}>+ 3</button>
+        <button style={{backgroundColor: 'lightgreen', margin: 5, padding: 5}} 
+                      onClick={this.plusOne}>+ 1</button>
+        <button style={{backgroundColor: 'lightgreen', margin: 5, padding: 5}} 
+                      onClick={this.plusTwo}>+ 2</button>
+        <button style={{backgroundColor: 'lightgreen', margin: 5, padding: 5}} 
+                      onClick={this.plusThree}>+ 3</button>
       </div>
     )
   }
